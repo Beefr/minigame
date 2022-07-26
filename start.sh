f88db1c658362904ddc06a44f85dbe1054dac0cf
Beefr <coreno@laposte.net>
2022-07-26 21:48:27 +0200

    services

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 
 kubectl apply -f frontend/persistent-volume.yaml
 kubectl apply -f backend/persistent-volume.yaml
-kubectl apply -f service.yaml
+kubectl apply -f frontend/service-front.yaml
+kubectl apply -f backend/service-back.yaml
 kubectl apply -f frontend/frontend.yaml
 kubectl apply -f anog-backend/anog.yaml
