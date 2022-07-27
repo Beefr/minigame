14b9523773ae6f639d93cbdbdb49b0dd3a0b8307
RENAULT Corentin <corentin.renault@soprasteria.com>
2022-07-27 16:34:26 +0200

    desactivating some backend cont

--- start.sh
+++ start.sh
@@ -2,10 +2,10 @@
 
 
 kubectl apply -f frontend/persistent-volume.yaml
-kubectl apply -f backend/persistent-volume.yaml
+#kubectl apply -f backend/persistent-volume.yaml
 
 kubectl apply -f frontend/service-front.yaml
-kubectl apply -f backend/service-back.yaml
+#kubectl apply -f backend/service-back.yaml
 
 kubectl apply -f frontend/frontend.yaml
-kubectl apply -f anog-backend/anog.yaml
+#kubectl apply -f anog-backend/anog.yaml
